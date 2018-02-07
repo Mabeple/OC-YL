f91b2eaca393dc5a57868ef2ba96010f3a595766
Mabeple <87548701@qq.com>
2018-02-07 15:41:25 +0800

    first commit

---
+++ OC-YL/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OC-YL
+//
+//  Created by melon on 2018/2/7.
+//  Copyright © 2018年 melon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
